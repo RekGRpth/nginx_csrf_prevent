93c682130caf792bf423b5359f49e6a8ea80325e
RekGRpth <rekgrpth@gmail.com>
2020-04-02 08:19:43 +0500

    main

--- ngx_http_csrf_prevent_filter_module.c
+++ ngx_http_csrf_prevent_filter_module.c
@@ -15,7 +15,7 @@ static char* ngx_http_csrf_prevent_filter_merge_conf(ngx_conf_t *cf, void* paren
 static ngx_command_t ngx_http_csrf_prevent_filter_commands[] = {
 	{
 		ngx_string("csrf_prevent"),
-		NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF|NGX_CONF_FLAG,
+		NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF|NGX_CONF_FLAG,
 		ngx_conf_set_flag_slot,
 		NGX_HTTP_LOC_CONF_OFFSET,
 		offsetof(ngx_csrf_prevent_conf_t, enable),
